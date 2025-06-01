da6f0521225de3730be85487cd451b33d1f9bf57
Luan Amaral <luanamaral.6540@hotmail.com>
2025-06-01 09:41:03 -0300

    criacão de adição de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -26,14 +27,47 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      // PRECISO ATUALIZAR A TAREFA
+      if (taskId == task.id) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // NÃO PRECISO ATUALIZAR A TAREFA
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    return setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTasks = {
+      id: v4(),
+      title: title,
+      description: description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTasks]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-5">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTask />
-        <Tasks tasks={tasks} />
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,28 @@
+import { ChevronRightIcon, Trash2Icon } from "lucide-react";
+
 function Tasks(props) {
   return (
-    <ul className="space-y-4">
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
-        <li className="bg-slate-400 text-white p-2 rounded-md">{task.title}</li>
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 w-full text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-me text-white">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => props.onDeleteTaskClick(task.id)}
+            className="bg-slate-400 p-2 rounded-me text-white"
+          >
+            <Trash2Icon />
+          </button>
+        </li>
       ))}
     </ul>
   );
